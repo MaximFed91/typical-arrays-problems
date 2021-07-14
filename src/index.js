5b45b271fb9b4279ef71f6491c4e59b7e2b7ad0e
Maksim <honestliar91@gmail.com>
2021-07-14 15:30:03 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
+ 
+exports.min = function min (arr) {
+if (arr === undefined || arr.length < 1){
   return 0;
+} else {
+  let min = arr[0];
+  arr.forEach(item => {
+    if (item < min) {
+      min = item;
+    }
+  });
+  return min;
+}
+  
 }
 
-exports.max = function max (array) {
+exports.max = function max (arr) {
+ if (arr === undefined || arr.length < 1){
   return 0;
+} else {
+  let max = arr[0];
+  arr.forEach(item => {
+    if (item > max) {
+      max = item;
+    }
+  });
+  return max;
+  
+}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr === undefined || arr.length < 1){
+    return 0;
+  } else {
+ return arr.reduce((sum, i) => sum + i) / arr.length;
+    
+  }
 }
